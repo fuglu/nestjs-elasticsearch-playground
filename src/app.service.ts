1252e4fa233d0fafc3089131a922d28d5685519e
Benjamin Kluck <ben@fug.lu>
2021-07-06 00:48:45 +0200

    ✅ Add some basic elasticsearch operations

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,6 +11,11 @@ export class AppService {
   constructor(private readonly elasticsearchService: ElasticsearchService) {}
 
   async getTodos(): Promise<Todo[]> {
+    await this.deleteIndex(INDEX);
+    await this.createIndex(INDEX);
+    await this.addLowercaseNormalizer(INDEX);
+    await this.updateMapping(INDEX);
+
     const todo: Todo = {
       id: '1',
       name: 'Test elasticsearch',
@@ -34,6 +39,124 @@ export class AppService {
 
     this.logger.debug(getResult);
 
-    return [todo];
+    const searchResult = await this.elasticsearchService.search<Todo>({
+      index: INDEX,
+      body: {
+        query: {
+          match: {
+            title: 'test',
+          },
+        },
+      },
+    });
+
+    this.logger.debug(searchResult);
+
+    return searchResult.body.documents;
+  }
+
+  async createIndex(index: string): Promise<void> {
+    this.logger.log(`Creating ${index} index`);
+    await this.elasticsearchService.indices.create({ index });
+  }
+
+  async addLowercaseNormalizer(index: string): Promise<void> {
+    this.logger.log(`Closing ${index} index`);
+    await this.elasticsearchService.indices.close({ index });
+
+    this.logger.log(`Adding lowercase normalizer to ${index} index`);
+    await this.elasticsearchService.indices.putSettings({
+      index,
+      body: {
+        index: {
+          settings: {
+            analysis: {
+              normalizer: {
+                lowercase_normalizer: {
+                  type: 'custom',
+                  char_filter: [],
+                  filter: ['lowercase'],
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    this.logger.log(`Opening ${index} index`);
+    await this.elasticsearchService.indices.open({ index });
+  }
+
+  async updateMapping(index: string): Promise<void> {
+    this.logger.log(`Updating mapping for ${index} index`);
+    // id: string;
+    // name: string;
+    // description: string;
+    // done: boolean;
+    await this.elasticsearchService.indices.putMapping({
+      index,
+      body: {
+        properties: {
+          id: {
+            type: 'keyword',
+          },
+          title: {
+            type: 'text',
+            fields: {
+              keyword: {
+                type: 'keyword',
+              },
+              lowercase: {
+                type: 'keyword',
+                normalizer: 'lowercase_normalizer',
+              },
+              en: {
+                type: 'text',
+                analyzer: 'english',
+              },
+              de: {
+                type: 'text',
+                analyzer: 'german',
+              },
+            },
+          },
+          description: {
+            type: 'text',
+            fields: {
+              keyword: {
+                type: 'keyword',
+              },
+              lowercase: {
+                type: 'keyword',
+                normalizer: 'lowercase_normalizer',
+              },
+              en: {
+                type: 'text',
+                analyzer: 'english',
+              },
+              de: {
+                type: 'text',
+                analyzer: 'german',
+              },
+            },
+          },
+          done: {
+            type: 'boolean',
+          },
+        },
+      },
+    });
+  }
+
+  async deleteIndex(index: string): Promise<void> {
+    const exists = await this.elasticsearchService.indices.exists({ index });
+
+    if (exists.body) {
+      this.logger.log(`Deleting ${index} index`);
+      await this.elasticsearchService.indices.delete({ index });
+    } else {
+      this.logger.warn(`Index ${index} not found`);
+    }
   }
 }
